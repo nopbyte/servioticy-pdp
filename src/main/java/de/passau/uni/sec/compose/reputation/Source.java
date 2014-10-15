1c88b502565de6dea4de8ccfbc0f0ed4980a39ca
nopbyte <juandabytes@gmail.com>
2014-10-15 19:59:19 +0200

    Add source object for reputation

---
+++ src/main/java/de/passau/uni/sec/compose/reputation/Source.java
@@ -0,0 +1,26 @@
+package de.passau.uni.sec.compose.reputation;
+
+public class Source 
+{
+	private String soid;
+	
+	private String streamid;
+
+	public String getSoid() {
+		return soid;
+	}
+
+	public void setSoid(String soid) {
+		this.soid = soid;
+	}
+
+	public String getStreamid() {
+		return streamid;
+	}
+
+	public void setStreamid(String streamid) {
+		this.streamid = streamid;
+	}
+	
+	
+}
